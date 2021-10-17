f3033069d9f427cdff269f5463e106572630de13
Markosej11 <markosej11@gmail.com>
2021-10-16 20:45:17 -0400

    getters and setters added for sensor class

--- phase1/code/app/sensor.cpp
+++ phase1/code/app/sensor.cpp
@@ -1,7 +1,22 @@
 #include "../include/sensor.hpp"
 
 
-// double ackermann::Sensor::getActualHeading(){
-//     std::cout<<"getActualHeading called"<<std::endl;
-//     return _actualHeading;
-// }
+double ackermann::Sensor::getActualHeading(){
+    std::cout<<"getActualHeading called"<<std::endl;
+    return actual_heading_;
+}
+
+double ackermann::Sensor::getActualSpeed(){
+    std::cout<<"getActualSpeed called"<<std::endl;
+    return actual_speed_;
+}
+
+void ackermann::Sensor::setActualHeading(double actual_heading){
+    std::cout<<"setActualHeading called"<<std::endl;
+    actual_heading_ = actual_heading;
+}
+
+void ackermann::Sensor::setActualSpeed(double actual_speed){
+    std::cout<<"setActualSpeed called"<<std::endl;
+    actual_speed_ = actual_speed;
+}
