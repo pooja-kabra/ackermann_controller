e430b6115a4f863c7ea46eee8558486608ab079a
Markosej11 <markosej11@gmail.com>
2021-10-17 01:56:37 -0400

    constructors and destructors created for forward kinematics class

--- include/forwardkinematics.hpp
+++ include/forwardkinematics.hpp
@@ -5,6 +5,15 @@ namespace ackermann{
 
 class ForwardKinamatics {
     public:
+    // --constructors
+    ForwardKinamatics(double heading_error = 0, double speed_error = 0) : heading_error_{heading_error}, speed_error_{speed_error} {
+        std::cout<<"Constructor for Forwardkinematics class called"<<std::endl;
+    }
+
+    // --destructors
+    ~ForwardKinamatics(){
+        std::cout<<"Destructor for Forwardkinematics class called"<<std::endl;
+    }
 
     private:
     double heading_error_;
