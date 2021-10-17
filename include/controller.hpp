de426099ddce3cc37e1477778725206b6c656c54
Markosej11 <markosej11@gmail.com>
2021-10-17 01:04:54 -0400

    unit test created for controller class

--- include/controller.hpp
+++ include/controller.hpp
@@ -15,7 +15,7 @@ class Controller {
     double getKp();
 
     // --constructors
-    Controller(double goal_heading = 0, double goal_speed = 0) : goal_heading_{goal_heading}, goal_speed_{goal_speed} {
+    Controller(double goal_heading = 0, double goal_speed = 0, double ki = 0.01, double kp = 0.03) : goal_heading_{goal_heading}, goal_speed_{goal_speed}, ki_{ki}, kp_{kp} {
         std::cout<<"Constructor for Controller class called"<<std::endl;
     };
 
