4ba5c3824e4b248489f2a03448496ddd193737f6
Markosej11 <markosej11@gmail.com>
2021-10-17 00:39:55 -0400

    Skeleton code created for controller class

---
+++ include/controller.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+
+namespace ackermann{
+
+class Controller {
+
+    private:
+    double goal_heading_;
+    double goal_speed_;
+    double ki_;
+    double kp_;
+
+}; //--Controller Class
+} //--Namespace ackermann
