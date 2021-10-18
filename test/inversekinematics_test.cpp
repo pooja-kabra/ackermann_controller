b671835cbd7677dddb482ad0600af4de9cbb09c9
pooja-kabra <pkabra@terpmail.umd.edu>
2021-10-18 01:44:55 -0400

    added unit test skeleton for inverse kinematics class

---
+++ test/inversekinematics_test.cpp
@@ -0,0 +1,37 @@
+/**
+ * @file inversekinematics_test.cpp
+ * @author Pooja Kabra, Markose Jacob
+ * @brief This file tests the Inverse Kinematics class, checks if it calculates 
+ *        individual wheel angular speeds and wheel angles w.r.t. robot axis correctly
+ * @version 0.1
+ * @date 2021-10-16
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include <gtest/gtest.h>
+#include "../include/inversekinematics.hpp"
+#include "../include/robot.hpp"
+
+ackermann:: Robot robot;
+ackermann::InverseKinematics ik(robot, 33, 60);
+
+/**
+ * This test checks for correct individual wheel angles w.r.t. the robot axis
+ */
+TEST(test_inverseKinematics_1, check_wheel_angles) {
+  ik.calculateWheelAngles();
+  EXPECT_NEAR(robot.getInnerWheelHeading(), 37, 0.1);
+  EXPECT_NEAR(robot.getOuterWheelHeading(), 30, 0.1);
+}
+
+/**
+ * This test checks for correct individual wheel speeds
+ */
+TEST(test_inverseKinematics_2, check_wheel_speeds) {
+  ik.calculateWheelSpeed();
+  EXPECT_NEAR(robot.getInnerWheelSpeed(), 50, 0.1);
+  EXPECT_NEAR(robot.getOuterWheelSpeed(), 70, 0.1);
+
+}
